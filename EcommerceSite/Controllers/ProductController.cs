2ae67bd937ecfb0356ac142aa03c281a61727c50
Shoukat Malik <shoukatmali70@gmail.com>
2017-12-30 19:35:06 +0500

    Creating Models for product Searching

--- EcommerceSite/Controllers/ProductController.cs
+++ EcommerceSite/Controllers/ProductController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using EcommerceSite.Models;
 
 namespace EcommerceSite.Controllers
 {
@@ -20,7 +23,33 @@ namespace EcommerceSite.Controllers
         }
         public void getCategory()
         {
+            // Creating CatwiseProductList
+            List<CatWiseProduct> items = new List<CatWiseProduct>();
+            // Creating Sql connection object
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["NorthwindConnectionString"].ToString();
+            conn.Open();
+            // Creating SqlCommand Object
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
 
+            cmd.CommandType = CommandType.Text;
+
+            cmd.CommandText = "SELECT * FROM CatWiseProduct";
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                CatWiseProduct a = new CatWiseProduct();
+                a.CategoryName = reader["CategoryName"].ToString();
+                a.noofproduct = int.Parse(reader["noofproduct"].ToString());
+
+                items.Add(a);
+            }
+
+            ViewBag.Cat = items;
         }
     }
 }
+    
